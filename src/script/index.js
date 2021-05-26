1f678f50a17d54807722fd617cb260eb63e72e17
AndresQA <34174822+AndresQA@users.noreply.github.com>
2021-05-25 19:28:19 -0500

    added filters

--- src/script/index.js
+++ src/script/index.js
@@ -1,18 +1,5 @@
-/*
-$.ajax({
-    type: "POST",
-    url: "/src/data/data.json",
-    complete: (e, xhr, settings) => {
-
-    }
-}).done((data) => {
-    var obj = JSON.parseJSON(data)
-
-    console.log(obj, "JSON")
-
-    // etc
-
-});*/
+var filters = [];
+var data = undefined;
 
 fetch("/src/data/data.json")
     .then(response => response.json())
@@ -24,13 +11,9 @@ fetch("/src/data/data.json")
 
         console.log(result)
 
+        data = result;
 
-        const lista = document.querySelector(".list");
-
-        result.forEach(r => {
-            lista.appendChild(createHTMLItem(r))
-        });
-
+        updateList();
     })
 
 const createHTMLItem = ({
@@ -86,16 +69,31 @@ const createHTMLItem = ({
 
             </div>
 `
+    const onClickFilter = (name) => {
+        let find = false;
+        filters.forEach((f) => {
+            if (f === name) {
+                find = true;
+            }
+        })
+        if (find === false) {
+            filters.push(name);
+            updateFilters();
+            updateList();
+        }
+    }
 
     const listFilter = container.querySelector(".list__itemFilters");
     languages.forEach(l => {
         const p = document.createElement("p");
+        p.addEventListener('click', () => onClickFilter(l));
         p.innerText = l
         listFilter.appendChild(p)
     })
-    
-    tools.forEach(t =>{
+
+    tools.forEach(t => {
         const p = document.createElement("p");
+        p.addEventListener('click', () => onClickFilter(t));
         p.innerText = t
         listFilter.appendChild(p)
     })
@@ -106,3 +104,85 @@ const createHTMLItem = ({
 
     return container;
 }
+
+const createHTMLFilter = (name) => {
+
+    const listContainer = document.querySelector(".filters__actives");
+
+    const container = document.createElement("div");
+    container.className = "filters__activesBox"
+
+
+    container.innerHTML = `
+    <p>${name}</p>
+    <div class="filters__activesClearIco">
+        <img src="./src/images/icon-remove.svg" alt="">
+    </div>
+`
+    const imgRemove = container.querySelector(".filters__activesClearIco");
+
+    imgRemove.addEventListener("click", () => {
+        listContainer.removeChild(container);
+        filters.forEach((f, index) => {
+            if (f === name) {
+                filters.splice(index, 1);
+            }
+        })
+        updateFilters();
+        updateList();
+    })
+
+
+
+    return container;
+}
+
+const updateFilters = () => {
+    const listContainer = document.querySelector(".filters__actives");
+    listContainer.innerHTML = "";
+
+    filters.forEach(f => {
+        listContainer.appendChild(createHTMLFilter(f))
+    })
+
+
+}
+
+const updateList = () => {
+    const lista = document.querySelector(".list");
+    lista.innerHTML = "";
+
+    data.forEach(d => {
+        var find = false;
+
+        filters.forEach(f => {
+            d.languages.forEach(l => {
+                if (l === f) {
+                    find = true;
+                }
+            })
+            d.tools.forEach(t => {
+                if (t === f) {
+                    find = true;
+                }
+            })
+        })
+
+
+        if (find === true || filters.length === 0) {
+            lista.appendChild(createHTMLItem(d))
+        }
+
+
+
+    });
+
+}
+
+const clear = document.querySelector(".clear");
+
+clear.addEventListener("click", () => {
+    filters = [];
+    updateFilters();
+    updateList();
+})
